96e3949c86d9c1770944ed500013acf79a3cd64a
Jesse Emond <emond.jesse@gmail.com>
2015-07-14 13:44:28 -0400

    + trial division implementation

--- primes.py
+++ primes.py
@@ -1,6 +1,11 @@
-import time
+import math
+
+# Primality test functions. Assume that "n" is odd and greater than 3.
+
+# Tests if 'n' is prime by trial division, by checking if it has a factor
+# k such that 1<k<=sqrt(n)
 def trial_division(n):
-    return False #TODO
+    return not(any(n % x == 0 for x in range(3, math.floor(math.sqrt(n))+1)))
 
 def miller_rabin(n):
     return False #TODO
