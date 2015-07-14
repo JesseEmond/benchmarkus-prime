4f21ecc9924779dba35a5dd728cf727f7ac694aa
Jesse Emond <emond.jesse@gmail.com>
2015-07-14 17:45:51 -0400

    ! clarified comment

--- primes.py
+++ primes.py
@@ -3,7 +3,7 @@ import math
 # Primality test functions. Assume that "n" is odd and greater than 3.
 
 # Tests if 'n' is prime by trial division, by checking if it has a factor
-# k such that 1<k<=sqrt(n)
+# k (1 < k <= sqrt(n))
 def trial_division(n):
     return not(any(n % x == 0 for x in range(3, math.floor(math.sqrt(n))+1)))
 
