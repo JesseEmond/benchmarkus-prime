5bb55897d40785409da88d87928cf4e2a2934b59
DrPandemic <bipbip500@gmail.com>
2015-07-19 15:07:13 -0400

    + tested value in result

--- benchmark.py
+++ benchmark.py
@@ -9,31 +9,39 @@ def random_odd_number(min_bits, max_bits):
     max_val = pow(2,max_bits) + 1
     return random.SystemRandom().randrange(min_val, max_val, 2)
 
-def benchmark(name, callback, samples):
+
+def time_function_call(test):
+    values = test()
+    timer = timeit.Timer(values[0])
+
+    return (timer.timeit(1), values[1])
+
+def benchmark(name, test, samples):
     print()
     print('starting benchmark for "', name, '":')
 
-    t = timeit.Timer(callback)
-    times = t.repeat(samples,1)
+    times = [time_function_call(test) for i in range(samples)]
 
     print('end of benchmark for "', name, '".')
     print()
 
     return times
 
-def make_prime_test_callback(primality_test, min_bits, max_bits):
-    return lambda: primality_test(random_odd_number(min_bits, max_bits))
+def make_prime_test(primality_test, min_bits, max_bits):
+    def generate():
+        odd_number = random_odd_number(min_bits, max_bits)
+        return (lambda: primality_test(odd_number), odd_number)
 
-
-
-
+    return generate
 
 SAMPLES  = 1
 MIN_BITS = 512
 MAX_BITS = 1024
 
-trial_division   = benchmark('Trial Division', make_prime_test_callback(primes.trial_division, MIN_BITS, MAX_BITS), SAMPLES)
-miller_rabin     = benchmark('Miller-Rabin', make_prime_test_callback(primes.miller_rabin, MIN_BITS, MAX_BITS), SAMPLES)
-solovay_strassen = benchmark('Solovay-Strassen', make_prime_test_callback(primes.solovay_strassen, MIN_BITS, MAX_BITS), SAMPLES)
+trial_division   = benchmark('Trial Division', make_prime_test(primes.trial_division, MIN_BITS, MAX_BITS), SAMPLES)
+miller_rabin     = benchmark('Miller-Rabin', make_prime_test(primes.miller_rabin, MIN_BITS, MAX_BITS), SAMPLES)
+solovay_strassen = benchmark('Solovay-Strassen', make_prime_test(primes.solovay_strassen, MIN_BITS, MAX_BITS), SAMPLES)
+
+print(trial_division)
 
 # TODO output data to file for graph-generator to use?
